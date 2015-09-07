2f2addfdf08038225e8854dc8a223d728a0f7c1c
Peeter Joot <peeter.joot@gmail.com>
2015-09-07 11:25:02 -0400

    new file:   contourFig.jl

---
+++ phy1520/contourFig.jl
@@ -0,0 +1,8 @@
+# scales are distorted in this plot.  wanted two lines in this figure too.
+
+Pkg.add("PyPlot")
+using PyPlot
+theta = collect(-pi/4:0.1:0) ;
+x = 10 * cos(theta) ;
+y = 10 * sin(theta) ;
+plot(x, y, color="red", linewidth=2.0, linestyle="--")
