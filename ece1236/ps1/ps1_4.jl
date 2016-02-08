91723d58364713721fd770a70227f18938f43135
Peeter Joot <peeterjoot@protonmail.com>
2016-02-08 08:35:26 -0800

    modified:   ../../METADATA modified:   ps1_4.jl

--- ece1236/ps1/ps1_4.jl
+++ ece1236/ps1/ps1_4.jl
@@ -6,10 +6,39 @@ Ghz = 1e9 ;
 dOverw = 0.1 ;
 sigma = 3.538e7 ;
 mu = pi * 4e-7 ;
-f = 30 ;
+epsilon = 8.8e-12 ;
+eta = sqrt( mu/epsilon ) ;
+f = 30 * Ghz ;
 C = epsilon/dOverw ;
 L = mu * dOverw ;
-Z = sqrt(L/C) ;
+omega = 2 * pi * f ;
 
-# part f)
+# part f)  Assuming R' = 0
+Z = sqrt(L/C) ;
 println( "Z_0 = $Z" ) ;
+
+# good conductor?
+omegaEpsilon = omega * epsilon ;
+
+println( "Good conductor if: $sigma \\gg $omegaEpsilon" ) ;
+
+# part g)
+function r(d)
+   dOverw * sqrt( pi * f * mu/sigma )/ d ;
+end
+
+function gamma(d)
+   R = r(d) ;
+
+   jomega = omega * im ;
+
+   sqrt( (R + jomega * L) * ( G + jomega * C) ) ;
+end
+
+for d in [1e-2, 1e-3, 1e-6]
+   g = gamma( d ) ;
+   alpha = real( g ) ;
+   z = r(d)/g ;
+
+   @printf( "d = %g ; \\alpha(d) = %g ; Z_0(d) = %g + %g j\n", d, alpha, real(z), imag(z) ) ;
+end
