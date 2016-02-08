19e2792706f8a0d8d18bfc85c7577471faede8b9
Peeter Joot <peeter.joot@gmail.com>
2016-02-08 10:17:12 -0500

    new file:   ps1_4.jl

---
+++ ece1236/ps1/ps1_4.jl
@@ -0,0 +1,15 @@
+# ps 1, (4).
+
+c = 3e9 ;
+G = 0 ;
+Ghz = 1e9 ;
+dOverw = 0.1 ;
+sigma = 3.538e7 ;
+mu = pi * 4e-7 ;
+f = 30 ;
+C = epsilon/dOverw ;
+L = mu * dOverw ;
+Z = sqrt(L/C) ;
+
+# part f)
+println( "Z_0 = $Z" ) ;
